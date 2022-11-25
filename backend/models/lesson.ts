53ac88bdce9cbaa4825a70bb1d4f63d0a6c19517
AustinMoses <austinmosesdev@gmail.com>
2022-11-25 10:40:29 -0700

    define data type

---
+++ backend/models/lesson.ts
@@ -0,0 +1,14 @@
+export type Lesson = {
+    url: string,
+    manualTags: string[],
+    abstractNote: string,
+    date: string,
+    dateAdded: string,
+    dateModified: string,
+    accessDate: string,
+    key: string,
+    itemType: string,
+    publicationYear: number,
+    author: string,
+    title: string,
+}
