a26c81bff2a5895529cb1af394ccaf02aacefe6b
AustinMoses <austinmosesdev@gmail.com>
2022-11-25 10:23:14 -0700

    Router and index setup

---
+++ backend/routers/lesson.router.ts
@@ -0,0 +1,24 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+ import express, { Request, Response } from 'express';
+ /**
+  * Router Definition
+  */
+ 
+ export const lessonRouter = express.Router();
+ /**
+  * Controller Definitions
+  */
+ 
+ // GET
+
+ // POST
+
+ // PUT
+
+ // PATCH
+
+ // DELETE
+ 
