51adeba40bf199b237955fe53e21515b5873cf06
AustinMoses <austinmosesdev@gmail.com>
2022-11-28 17:19:54 -0700

    Added background color. Needs container height update and responsive update

--- sagely-frontend/src/App.js
+++ sagely-frontend/src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LessonList from './pages/lessonsList';
 import LessonDetail from './pages/lesson';
 import './App.css';
-import Header from './components/header'
+import Header from './components/header';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<LessonList />} />
-        <Route path="/details" element={<LessonDetail />} />
-      </Routes>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<LessonList />} />
+          <Route path="/details" element={<LessonDetail />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
